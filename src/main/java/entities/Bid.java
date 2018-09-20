3123ff04aa2192f6d0edb017774cb2fa88a5ca75
runi95 <rse005@uib.no>
2018-09-20 16:25:04 +0200

    Implemented JPA classes based on Domain Model

---
+++ src/main/java/entities/Bid.java
@@ -0,0 +1,43 @@
+package entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bid")
+public class Bid {
+
+	@Id
+	private int id;
+	
+	private Double amount;
+	
+	private LocalDateTime time;
+	
+	private Auction auction;
+	
+	private User user;
+	
+	@ManyToOne
+    @JoinColumn(name="id", nullable=false)
+    public Auction getAuction() { return this.auction; }
+	
+	@ManyToOne
+    @JoinColumn(name="id", nullable=false)
+    public Auction getUser() { return this.user; }
+	
+	public int getId() { return this.id; }
+	
+	public double getAmount() { return this.amount; }
+	
+	public void setAmount(double amount) { this.amount = amount; }
+	
+	public LocalDateTime getTime() { return this.time; }
+	
+	public void setTime(LocalDateTime time) { this.time = time; }
+}
