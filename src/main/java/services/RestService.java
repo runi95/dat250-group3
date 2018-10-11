51c89349775d775bf0f0e65cbd3677c9f6137175
Simon Halvorsen <halvorsen.simon@gmail.com>
2018-10-11 17:53:51 +0200

    Refactoring

--- src/main/java/services/RestService.java
+++ src/main/java/services/RestService.java
@@ -1,85 +1,71 @@
 package services;
 
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
-import javax.persistence.TypedQuery;
-import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
+import beans.AuctionDao;
 import entities.Auction;
-import entities.Auctions;
 import entities.Bid;
-import entities.Bids;
 
 
-/*@Path("/rest")
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.net.URI;
+import java.util.List;
+
+@Path("/auctions")
 @Stateless
-public class RestService {
-	
-	@EJB
-	DAO dao;
-	
-	@GET
-	@Path("auctions")
-	@Produces(MediaType.APPLICATION_XML)
-	public Response getAuctions() {
-		TypedQuery<Auction> query = dao.createNamedAuctionQuery();
-		Auctions auctions = new Auctions(query.getResultList());
-		
-		return Response.ok(auctions).build();
-	}
+public class RestService extends Application {
 
-	@GET
-	@Path("auctions/{id}")
-	public Response getAuction(@PathParam("id") String id) {
-		int idInt = Integer.parseInt(id);
-		
-		Auction auction = dao.findAuction(idInt);
-		if (auction == null)
-			throw new NotFoundException();
-		
-		return Response.ok(auction).build();
-	}
-	
-	@GET
-	@Path("auctions/{id}/bids")
-	public Response getAuctionBids(@PathParam("id") String id) {
-		TypedQuery<Bid> query = dao.createNamedBidQuery();
-		Bids bids  = new Bids(query.getResultList());
-		
-		return Response.ok(bids).build();
-	}
-	
-	@GET
-	@Path("auctions/{aid}/bids/{bid}")
-	public Response getAuctionBid(@PathParam("aid") String aid, @PathParam("bid") String bid) {
-		int bidInt = Integer.parseInt(bid);
-		
-		Bid bidClass = dao.findBid(bidInt);
-		if (bidClass == null)
-			throw new NotFoundException();
-		
-		return Response.ok(bidClass).build();
-	}
-	
-	// TODO: Figure this shit out.
-	@POST
-	@Path("auctions/{id}/bids")
-	public Response setAuctionBid(@PathParam("id") String id, Bid bid) {
-		int idInt = Integer.parseInt(id);
-		
-		Auction auction = dao.findAuction(idInt);
-		if (auction == null)
-			throw new NotFoundException();
-		
-		dao.persistBid(bid);
-		
-		return Response.ok().build();
-	}
-}*/
+    @Inject
+    private AuctionDao auctionDao;
+
+    @Context
+    UriInfo uri;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAuctions(){
+        return Response.ok(auctionDao.findAll()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAuction(@PathParam("id") int id) {
+        return Response.ok(auctionDao.find(id)).build();
+    }
+
+    @GET
+    @Path("{id}/bids/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBids(@PathParam("id") int id){
+        return Response.ok(auctionDao.find(id).getBids()).build();
+    }
+
+    @GET
+    @Path("{id}/bids/{bid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBid(@PathParam("id") int id, @PathParam("bid") int bid) {
+        Response res = null;
+        List<Bid> bids = auctionDao.find(id).getBids();
+        for (Bid b : bids) {
+            if (b.getId() == bid)  {
+                res = Response.ok(b).build();
+            }
+        }
+        return res;
+    }
+
+    @POST
+    @Path("{id}/bids")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response placeBid(@PathParam("id") int id, int value) {
+        Auction auction = auctionDao.find(id);
+        Bid bid = new Bid(value);
+        auction.addBid(bid);
+        URI bidUri = UriBuilder.fromUri(uri.getBaseUri()).path("auctions").path(Integer.toString(id)).
+                path("bids").path(Integer.toString(bid.getId())).build();
+        return Response.created(bidUri).entity(bid).build();
+    }
+}
