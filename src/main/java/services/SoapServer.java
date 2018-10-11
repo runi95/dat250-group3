f85df76ebcea688f346c969cbc621568388cbcd6
runi95 <rse005@uib.no>
2018-10-11 19:23:26 +0200

    Began the work on our SoapService

---
+++ src/main/java/services/SoapServer.java
@@ -0,0 +1,21 @@
+package services;
+
+import entities.Auction;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface SoapServer {
+
+    @WebMethod
+    Auction getAuctionById(int id);
+
+    @WebMethod
+    Auction[] getOpenAuctions();
+
+    @WebMethod
+    double bidOnAuction(int id, int amount);
+}
