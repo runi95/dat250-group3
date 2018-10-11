12a67260ac20366ae4d68f607d38842230b905f8
Folb <oys.follo@gmail.com>
2018-10-11 17:57:25 +0200

    Fixed post

--- src/main/java/services/TestRest.java
+++ src/main/java/services/TestRest.java
@@ -1,6 +1,8 @@
 package services;
 
 import beans.AuctionDao;
+import beans.BidDao;
+import beans.UserDao;
 import entities.Auction;
 import entities.Bid;
 
@@ -10,6 +12,7 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Path("/auctions")
@@ -18,6 +21,12 @@ public class TestRest extends Application {
 
     @Inject
     private AuctionDao auctionDao;
+
+    @Inject
+    private UserDao userDao;
+
+    @Inject
+    private BidDao bidDao;
 
     @Context
     UriInfo uri;
@@ -60,11 +69,16 @@ public class TestRest extends Application {
     @Path("{id}/bids")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response placeBid(@PathParam("id") int id, int value) {
+    public Response placeBid(@PathParam("id") int id,
+                             @HeaderParam("amount") double amount,
+                             @HeaderParam("user-id") int userID) {
         Auction auction = auctionDao.find(id);
-        Bid bid = new Bid(value);
-        auction.addBid(bid);
-        URI bidUri = UriBuilder.fromUri(uri.getBaseUri()).path("auctions").path(Integer.toString(id)).path("bids").path(Integer.toString(bid.getId())).build();
-        return Response.created(bidUri).entity(bid).build();
+        Bid bid = new Bid();
+        bid.setUser(userDao.find(userID));
+        bid.setAuction(auctionDao.find(id));
+        bid.setAmount(amount);
+        bid.setTime(LocalDateTime.now());
+        bidDao.persist(bid);
+        return getBids(id);
     }
 }
