c4a2df80e076a5327df7c1a9d2b96e47b58a6874
Folb <oys.follo@gmail.com>
2018-10-11 19:21:01 +0200

    Almost everything in restservice works

--- src/main/java/services/RestService.java
+++ src/main/java/services/RestService.java
@@ -3,19 +3,24 @@ package services;
 import beans.AuctionDao;
 import beans.BidDao;
 import beans.UserDao;
+import com.google.common.collect.Iterables;
 import entities.Auction;
 import entities.Bid;
+import entities.User;
 
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.print.attribute.standard.Media;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
-@Path("/auctions")
+@Path("")
 @Stateless
 public class RestService extends Application {
 
@@ -38,21 +43,21 @@ public class RestService extends Application {
     }
 
     @GET
-    @Path("/{id}")
+    @Path("/auctions/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAuction(@PathParam("id") int id) {
         return Response.ok(auctionDao.find(id)).build();
     }
 
     @GET
-    @Path("{id}/bids/")
+    @Path("/auctions/{id}/bids")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBids(@PathParam("id") int id){
         return Response.ok(auctionDao.find(id).getBids()).build();
     }
 
     @GET
-    @Path("{id}/bids/{bid}")
+    @Path("/auctions/{id}/bids/{bid}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBid(@PathParam("id") int id, @PathParam("bid") int bid) {
         Response res = null;
@@ -66,7 +71,7 @@ public class RestService extends Application {
     }
 
     @POST
-    @Path("{id}/bids")
+    @Path("/auctions/{id}/bids")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response placeBid(@PathParam("id") int id,
@@ -76,14 +81,71 @@ public class RestService extends Application {
 
         Bid bid = new Bid();
         bid.setUser(userDao.find(userID));
-        // bid.setAuction(auctionDao.find(id));
         bid.setAmount(amount);
         bid.setTime(LocalDateTime.now());
 
         auction.addBid(bid);
-        // bidDao.persist(bid);
         auctionDao.edit(auction);
 
         return getBids(id);
+    }
+
+    @POST
+    @Path("/user/createuser")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createUser(@HeaderParam("u_name") String uName,
+                               @HeaderParam("f_name") String fName,
+                               @HeaderParam("l_name") String lName,
+                               @HeaderParam("pwd") String pwd,
+                               @HeaderParam("email") String email) {
+        User u = new User();
+        u.setUserName(uName);
+        u.setName(fName);
+        u.setLastName(lName);
+        u.setPassword(pwd);
+        u.setEmail(email);
+        u.setComments(new HashSet<>());
+        u.setNumberOfRatings(0);
+        u.setSumOfAllRatings(0);
+
+        userDao.persist(u);
+        return Response.ok(Iterables.getLast(userDao.findAll())).build();
+    }
+
+    @GET
+    @Path("/user/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUser(@PathParam("id") int id) {
+        return Response.ok(userDao.find(id)).build();
+    }
+
+    @POST
+    @Path("/auctions/create")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createAuction(@HeaderParam("user_id") int userID) {
+        Auction auction = new Auction();
+        auction.setSeller(userDao.find(userID));
+        auction.setLastBid(0);
+        ArrayList<Bid> bids = new ArrayList<>();
+        bids.add(new Bid(0));
+        auction.setBids(bids);
+        auctionDao.persist(auction);
+        return getMyAuctions(userID);
+    }
+
+    @GET
+    @Path("/auctions/self/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getMyAuctions(@PathParam("id") int id) {
+        List<Auction> auctions = new ArrayList<>();
+        for (Auction auction : auctionDao.findAll()) {
+            if (auction.getSeller().getId() == id) {
+                auctions.add(auction);
+            }
+        }
+
+        return Response.ok(auctions).build();
     }
 }
