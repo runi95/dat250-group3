c970b2f02482da51614e18cc0de47a1165aa4198
runi95 <rse005@uib.no>
2018-10-25 15:24:00 +0200

    Started working on an auctions page

---
+++ src/main/java/beans/AuctionView.java
@@ -0,0 +1,39 @@
+package beans;
+
+import dao.AuctionDao;
+import dao.UserEJB;
+import entities.Auction;
+import entities.User;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SessionScoped
+@ManagedBean
+public class AuctionView implements Serializable {
+    private static final long serialVersionUID = 5443351151396868724L;
+
+    private static Logger log = Logger.getLogger(AuctionView.class.getName());
+
+    @Inject
+    private AuctionDao auctionDao;
+
+    public AuctionView() {}
+
+    public Auction[] getAuctions() {
+        return auctionDao.findAllOpenAuctions().toArray(new Auction[]{});
+    }
+
+}
